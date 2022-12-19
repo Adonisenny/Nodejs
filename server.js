c2933dc9803475dd949bcdb638cea8f6371535bc
Adonisenny <funkmater@gmail.com>
2022-12-19 06:02:45 -0800

    my first node commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs =require('fs')
+const server = http.createServer((req,res) => {
+    
+    //set header content type
+   res.setHeader('Content-Type','text/html')
+   let path = './views/';
+   switch(req.url){
+    case '/':
+    path += 'index.html';
+    res.statusCode = 200
+    break;
+    case '/aboutMe':
+    path += 'aboutMe.html'
+    res.statusCode=200;
+    break;
+    default:
+        path +='404.html'
+        res.statusCode=404;
+        break;
+}
+
+ 
+
+   fs.readFile(path, (err,data)=> {
+    
+    if(err){
+        console.log(err)
+        res.end()
+    }
+    
+        // res.write(data)
+        res.end(data)
+
+    
+   
+   })
+  
+});
+server.listen(3003,'localhost',() => {
+
+     console.log('listening for request')
+ })  
